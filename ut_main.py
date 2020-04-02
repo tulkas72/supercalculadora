f95c74af70ee59f911f9d54ac3c47e75527f1093
jmsa <tulkas72@gmail.com>
2020-04-03 01:29:01 +0200

    renombrado ut_supercalculadora.py a ut_main.py, añadido nuevo ut_supercalculadora.py, movidos test desde ut_main..py a ut_supercalculadora.py

---
+++ ut_main.py
@@ -0,0 +1,16 @@
+
+import unittest
+import ut_calculadora
+import ut_expr_aritmetica
+import ut_supercalculadora
+import calculadora
+import expr_aritmetica
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ut_calculadora.TestCalculadora))
+    suite.addTest(unittest.makeSuite(ut_expr_aritmetica.TestsExprAritmetica))
+    suite.addTest(unittest.makeSuite(
+        ut_supercalculadora.TestsSupercalculadora))
+    unittest.TextTestRunner(verbosity=3).run(suite)
--- ut_supercalculadora.py
+++ ut_supercalculadora.py
@@ -1,21 +1,12 @@
-
 import unittest
+import supercalculadora
+import expr_aritmetica
 import ut_calculadora
 import ut_expr_aritmetica
-import calculadora
-import expr_aritmetica
 
 
-class TestCalculadora(unittest.TestCase):
+class TestsSupercalculadora(unittest.TestCase):
     def test_sumar(self):
-        sc = calculadora.Calculadora(
+        sc = supercalculadora.Supercalculadora(
             expr_aritmetica.ExprAritmetica())
         self.assertEqual("4", sc.calcular("2 + 2"))
-
-
-if __name__ == "__main__":
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ut_calculadora.TestCalculadora))
-    suite.addTest(unittest.makeSuite(ut_expr_aritmetica.TestsExprAritmetica))
-    suite.addTest(unittest.makeSuite(TestCalculadora))
-    unittest.TextTestRunner(verbosity=3).run(suite)
